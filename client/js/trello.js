56220602166ba937a55b3d0d9d2f80c62a9b8d6e
Angelo Polo <polo.language@gmail.com>
2015-07-14 23:51:00 -0400

    List drag-drop implemented.

--- client/js/trello.js
+++ client/js/trello.js
@@ -7,7 +7,7 @@
     var nextId = 0
 
     this.title = title
-    this.lists = [new List(undefined, 'Add a list...', true)]
+    this.lists = []
     this.cards = {}
     
     this.node = document.createElement('div')
@@ -20,9 +20,15 @@
 
     // new list title form
     this.titleFormNode = buildListTitleForm()
-    
-    // add lists
     this.titleNode.appendChild(document.createTextNode(this.title))
+
+    this.getNextId = function() {
+      return '_' + (nextId++).toString()
+    }
+  }
+
+  Board.prototype.render = function () {
+    this.lists.push(new List(this, 'Add a list...', 0, true))
     for (var i = 0; i < this.lists.length; ++i) {
       this.listsNode.appendChild(this.lists[i].node)
     }
@@ -30,10 +36,6 @@
     this.lists[this.lists.length-1].titleNode.onclick = addList(this)
     this.node.appendChild(this.titleNode)
     this.node.appendChild(this.listsNode)
-
-    this.getNextId = function() {
-      return '_' + (nextId++).toString()
-    }
   }
 
   function buildListTitleForm() {
@@ -58,16 +60,18 @@
       function titleButtonClick(evt) {
         evt.preventDefault()
         var title = titleInput.value.trim()
+          , index = board.lists.length - 1
           , list
 
         board.titleFormNode.style.display = 'none'
         titleInput.value = ''
         if (!title) { return }
 
-        list = new List(board, title)
-        board.lists.splice(board.lists.length-1, 0, list)
+        list = new List(board, title, index)
+        board.lists.splice(index, 0, list)
         board.listsNode.insertBefore(list.node,
-                                     board.lists[board.lists.length-1].node)
+                                     board.lists[index+1].node)
+        board.lists[index+1].titleNode.setAttribute('list-index', index+1)
       }
     }
   }
@@ -93,21 +97,25 @@
 
   //////////////////////////////////////////////////////////////////////////
   //// List
-  function List(board, title, dummyList) {
+  function List(board, title, index, dummyList) {
     this.board = board
     this.dummyList = dummyList
     this.title = title
+    this.index = index
     this.node = document.createElement('div')
     this.titleNode = document.createElement('div')
     this.cardsNode = document.createElement('div')
     this.node.classList.add('list')
     this.titleNode.classList.add('list-title')
     this.cardsNode.classList.add('list-cards')
+    this.titleNode.setAttribute('list-index', index)
     this.titleNode.appendChild(document.createTextNode(this.title))
     this.node.appendChild(this.titleNode)
     
     if (!dummyList) {
       var dummyCard = new Card(this, 'Add a card...', 0)
+
+      this.titleNode.draggable = true
       this.cards = [dummyCard]
       board.registerCard(this.cards[0], 0)
 
@@ -122,6 +130,51 @@
       dummyCard.node.appendChild(this.titleFormNode)
       dummyCard.node.draggable = false
       dummyCard.node.onclick = undefined
+    }
+
+    // drag-drop handlers
+    this.titleNode.ondragstart = function (evt) {
+      var index = parseInt(evt.target.getAttribute('list-index'), 10)
+      dragTracker.list = currentBoard.lists[index]
+      evt.dataTransfer.effectAllowed = 'move'
+    }
+
+    this.titleNode.ondragover = function (evt) {
+      if (dragTracker.list) {
+        evt.preventDefault()
+      }
+    }
+
+    this.titleNode.ondrop = function (evt) {
+      var sourceIndex = dragTracker.list.index
+        , targetIndex = parseInt(this.getAttribute('list-index'), 10)
+        , numLists = board.lists.length
+        , i
+
+      if (sourceIndex === targetIndex) { return }
+
+      board.listsNode.removeChild(dragTracker.list.node)
+      board.listsNode.insertBefore(dragTracker.list.node,
+                                   board.lists[targetIndex].node)
+
+      for (i = sourceIndex; i < numLists-1; ++i) {
+        board.lists[i] = board.lists[i+1]
+        board.lists[i].titleNode.setAttribute('list-index', i)
+        board.lists[i].index = i
+      }
+      for (i = numLists-1; i > targetIndex; --i) {
+        board.lists[i] = board.lists[i-1]
+        board.lists[i].titleNode.setAttribute('list-index', i)
+        board.lists[i].index = i
+      }
+      board.lists[targetIndex] = dragTracker.list
+      board.lists[targetIndex].titleNode.setAttribute('list-index', targetIndex)
+      board.lists[targetIndex].index = targetIndex
+      evt.preventDefault()
+    }
+
+    this.titleNode.ondragend = function () {
+      dragTracker.list = undefined
     }
   }
 
@@ -178,6 +231,7 @@
     this.node.setAttribute('card-id', this.id)
     this.titleNode.appendChild(document.createTextNode(this.title))
 
+    // drag-drop handlers
     this.node.ondragstart = (function (id) {
       return function (evt) {
         dragTracker.id = id
@@ -197,6 +251,8 @@
           , targetId = this.getAttribute('card-id') // 'this' is target of drop
           , source = board.cards[id]
           , target = board.cards[targetId]
+
+        if (id === targetId) { return }
 
         source.list.cardsNode.removeChild(source.card.node)
         target.list.cardsNode.insertBefore(source.card.node, target.card.node)
@@ -240,8 +296,9 @@
 
   //////////////////////////////////////////////////////////////////////////
   //// Drag tracking
-  var dragTracker = {
-    id: undefined
+  var dragTracker =
+  { id: undefined
+  , list: undefined
   }
 
 
@@ -300,7 +357,6 @@
 
   document.getElementById('card-edit-delete').onclick = function () {
     var index = currentBoard.cards[cardEdit.card.id].index
-  console.log(cardEdit.card.list.cards.length + ': ' + index)
 
     currentBoard.unregisterCard(cardEdit.card)
     currentBoard.reregisterSubsequent(cardEdit.card.list, index + 1, -1)
@@ -352,7 +408,8 @@
   document.body.onload = function () {
     var title = 'New Board'
       , board = new Board(title)
-    
+
+    board.render()
     document.getElementById('container').appendChild(board.node)
     currentBoard = board
   }
